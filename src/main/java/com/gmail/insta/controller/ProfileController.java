b7fdd1be6dafc9497ee784eeda273fd44c17e13e
Ian Agranovich <agranovich.ian@gmail.com>
2018-08-17 02:46:13 +0300

    readme

--- src/main/java/com/gmail/insta/controller/ProfileController.java
+++ src/main/java/com/gmail/insta/controller/ProfileController.java
@@ -6,10 +6,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Optional;
 
-import javax.xml.ws.RequestWrapper;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.FileSystemResource;
@@ -17,7 +14,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
